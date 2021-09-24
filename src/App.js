80f39e0c016cb862219536a194dafd7416ae7ca5
raquel gonzalez <gonzalez.c.raquel@gmail.com>
2021-09-24 00:30:32 -0500

    add readme and remove console log

--- src/App.js
+++ src/App.js
@@ -7,25 +7,18 @@ import { Contact } from "./components/Contact";
 function App() {
   return (
     <div className="app__container vh-100">
-      {/* <h1>Portafolio</h1> */}
       <Router>
-        {/*  <Header /> */}
         <Switch>
           <Route exact path="/">
-            {/* <About /> */}
-            {/* efeewwefwerwe */}
             <Home />
           </Route>
           <Route exact path="/proyects">
-            {/* <h2>Proyectos</h2> */}
             <Work />
           </Route>
           <Route exact path="/about">
-            {/* <h2>Sobre mí</h2> */}
             <About />
           </Route>
           <Route exact path="/contact">
-            {/*  <h2>Contacto</h2> */}
             <Contact />
           </Route>
         </Switch>
